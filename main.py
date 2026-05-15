30d9b5310920e9761e096b98351d4490f8ab455f
Gurinder Singh <gurindersingh182004@gmail.com>
2026-05-15 11:10:50 -0400

    main func finalized

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import ai
+import parse
 from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 @app.post("/analyze")
 @app.get("/")
-async def read_main():
-    return {"msg": "Hello World"}
+async def analyze_resume(file: UploadFile = File(...)):
+
+    contents = await file.read()
+    text = parse.parse_pdf(contents)
+    result_json = ai.ai_analyze(text)
+    return result_json
+    
