569057a2639818789a6a1f9cb339b49e204017bb
Gurinder Singh <gurindersingh182004@gmail.com>
2026-05-20 12:19:21 -0400

    final touchs and final commit

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import ai
 import parse
 from fastapi import FastAPI, UploadFile, File
 
+
 app = FastAPI()
-@app.post("/analyze")
-@app.get("/")
+@app.post("/analyze")## having the thing run on POST http://127.0.0.1:8000/analyze
+## analyzed the resume that we get using fast api
 async def analyze_resume(file: UploadFile = File(...)):
 
     contents = await file.read()
